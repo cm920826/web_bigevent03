c73d8df3a4fdcad050fb75a8307bdd8087c84817
chenmin <850367838@qq.com>
2020-11-27 11:43:46 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,29 @@
 var baseURL = 'http://ajax.frontend.itheima.net'
 $.ajaxPrefilter(function (options) {
     options.url = baseURL + options.url
+
+    // 对需要权限的接口 以my开头的 配置头信息
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 拦截响应 判断身份认证信息 防止用户在上面地址栏输入就能进入首页中心
+    // complete 就相当于res 里面的method url 等 
+    // complete这个属性不管执行成功与否都会调用
+    
+    options.complete = function (res) {
+        // console.log(options);
+        // 输出的是{status: 1, message: "身份认证失败！"}  是一个对象
+        console.log(res.responseJSON);   
+        var obj = res.responseJSON
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            // 清空本地存储的token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面 不许进入主页中心
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+$(function () {
+
+    // 获取用户信息
+    getUserInfo()
+
+    // 右上角退出功能实现
+    $('#btnLogout').on('click', function () {
+        // 套用框架提供的询问框
+        layer.confirm('是否确认退出？', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 清空本地存储的token
+            localStorage.removeItem('token')
+            // 跳转到登录页面
+            location.href = '/login.html'
+            // 关闭询问框    框架自带的
+            layer.close(index);
+        });
+    })
+
+
+
+
+
+
+})
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            // layui.layer.msg(res.message)
+            renderAvatar(res.data)
+        }
+    })
+}
+
+
+// 渲染用户头像及用户名
+function renderAvatar(user) {
+    // 渲染名称
+    var name = user.nick || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 渲染头像
+    if (user.user_pic !== null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        // 没有头像
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avatar').show().html(text)
+    }
+}
